1b09badac34add5168ccb2b25ef42172a447892e
mizu4575 <115239291+mizu4575@users.noreply.github.com>
2023-12-04 01:14:26 +0900

    pokemon.javaの修正、Main1体目のみ情報を取れるようにしようとした(未完)。

--- app/src/main/java/jp/ac/meijou/android/mobileapp/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/mobileapp/MainActivity.java
@@ -35,11 +35,50 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.myPokemonButton.setOnClickListener(view->{
+            var mypoke1_name = binding.myPokemon1Name.getText().toString();
+            var mypoke1_url = Poke_URL;
+
+            pokemon mypoke1 = new pokemon(mypoke1_name, 1, 0);
+            mypoke1_url += mypoke1.getPoke_Number();
+
+            // リクエスト先にgistを指定
+            var request = new Request.Builder()
+                    .url(mypoke1_url)
+                    .build();
+
+            // 非同期通信でリクエスト
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    // 通信に失敗した時に呼ばれる
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    // レスポンスボディをGist型に変換
+                    var gist = gistJsonAdapter.fromJson(response.body().source());
+                    // 中身の取り出し
+                    Optional.ofNullable(gist)
+
+                            .ifPresent(gistFile -> {
+                                // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
+
+                                runOnUiThread(() -> binding.myPokemon1Score.setText(gistFile.stats.get(5).base_stat));
+                                //String res = gistFile.stats.get(0).toString();
+                                //runOnUiThread(() -> binding.textView.setText(res));
+
+                            });
+                }
+
+            });
+        });
+        /*
+        pokemon mypoke1 = new pokemon(binding.myPokemon1Name.getText().toString(), 1, 0);
 
         //URLの末尾に図鑑番号を足す
-        Poke_URL += Poke_Number;
+        Poke_URL += mypoke1.getPoke_Number();
         ////////////////////////////////////////////////////
-
 
         // リクエスト先にgistを指定
         var request = new Request.Builder()
@@ -63,15 +102,12 @@ public class MainActivity extends AppCompatActivity {
                         .ifPresent(gistFile -> {
                             // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
 
-
-                            //Viewに表示するところ。Listでデータを持っているのでうまく切り分けてください......
-
-                            runOnUiThread(() -> binding.textView.setText(gistFile.stats.get(5).base_stat));
+                            runOnUiThread(() -> binding.myPokemon1Score.setText(gistFile.stats.get(5).base_stat));
                             //String res = gistFile.stats.get(0).toString();
                             //runOnUiThread(() -> binding.textView.setText(res));
                           
                         });
             }
-        });
+        });*/
     }
 }
